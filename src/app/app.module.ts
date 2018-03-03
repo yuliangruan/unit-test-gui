3d3da1a03433d1d9800a85237953e72eb6dd81ae
Yuliang Ruan <yuliang@getuncommon.com>
2018-03-03 00:03:48 -0600

    new project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TestLibraryComponent } from './test-library/test-library.component';
+import { TestPackageComponent } from './test-package/test-package.component';
+import { TestModuleComponent } from './test-module/test-module.component';
+import { TestSetComponent } from './test-set/test-set.component';
+import { TestComponent } from './test/test.component';
+import { TestCaseComponent } from './test-case/test-case.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestLibraryComponent,
+    TestPackageComponent,
+    TestModuleComponent,
+    TestSetComponent,
+    TestComponent,
+    TestCaseComponent
   ],
   imports: [
     BrowserModule
